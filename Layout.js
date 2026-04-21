f715740f432107f6f69b8ac7c04612e6784f0163
givayomabilya-arch <givayomabilya@gmail.com>
2026-04-21 23:40:49 +0300

    Update Layout.js

--- Layout.js
+++ Layout.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { supabase } from '../lib/supabase'
+import { supabase } from '../lib/supabase' 
 
 const ROL_MENULERI = {
   yonetici: [
